4935191ab1490850ea391dd3d40631fffa7f4b00
unknown <>
2017-03-09 01:00:56 +0800

    interface designation for net adn protocol level

--- src/date/me/com/cn/net/NetTranslation.java
+++ src/date/me/com/cn/net/NetTranslation.java
@@ -1,5 +1,10 @@
 package date.me.com.cn.net;
+import java.io.InputStream;
+
+import org.json.*;
 
 public interface NetTranslation {
-           int i =1;
+	JSONObject GetJsonObject(InputStream inputstream);//输入流转换为Json
+	int SendJsonObject(JSONObject myJson);       //发送Json
+           
 }
